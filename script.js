a58c6bc7c9e48843f83430e719ef3aa930b24eae
SurafelFikadu <surafelfikadu05@gmail.com>
2024-11-21 07:30:16 +0300

    add advice id

--- script.js
+++ script.js
@@ -2,12 +2,14 @@ const API_ADVICE = 'https://api.adviceslip.com/advice';
 
 const advice = document.querySelector('#advice');
 const nextBtn = document.querySelector('#next-btn');
+const adviceId = document.querySelector('#advice-id');
 
 const getAdvice = async () => {
     try {
         const response = await fetch(API_ADVICE);
         const data = await response.json();
-        advice.innerHTML = data.slip.advice;
+        advice.innerHTML = `"${data.slip.advice}"`;
+        adviceId.innerHTML = `#${data.slip.id}`;
     } catch (error) {
         console.log('Error: ', error);
     }
